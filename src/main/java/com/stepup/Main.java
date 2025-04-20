8a0963db46951a4a55d4bfccd48d84cea3cea904
Dmitriy Lobastov <lobastovdmitri@gmail.com>
2025-04-20 16:58:44 +0300

    Улучшили логику main и doSomething()

--- src/main/java/com/stepup/Main.java
+++ src/main/java/com/stepup/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("11111");
+        System.out.println("---");
 
     }
 
     public static void doSomething() {
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(0);
+        System.out.println(0);
     }
 }
